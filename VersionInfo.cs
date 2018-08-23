797b857dc1dfb86b35b220cf46df3a406c495b7a
Amit Lavian <amitla@payoneer.com>
2018-08-23 16:16:09 +0300

    version bump

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyVersion("2.0.1")]
-[assembly: AssemblyFileVersion("2.0.1")]
-[assembly: AssemblyInformationalVersion("2.0.1")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
 [assembly: AssemblyCompany("PubComp")]
 [assembly: AssemblyProduct("Redis Repository")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
