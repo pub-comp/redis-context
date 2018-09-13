16eac20a600fd348b14c5f8f1cbb0e9889c93760
Amit Lavian <amitla@payoneer.com>
2018-09-13 11:22:05 +0300

    upgrade stack exchange redis to v2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 
 // This should be the same version as below
-[assembly: AssemblyVersion("2.1.0")]
-[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
 [assembly: AssemblyCompany("PubComp")]
 [assembly: AssemblyProduct("Redis Repository")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
