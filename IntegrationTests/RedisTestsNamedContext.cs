ef28a911ae30fed4316bba7599e3939f27044208
Amit Lavian <amitla@payoneer.com>
2019-02-17 17:06:16 +0200

    move to standard

---
+++ IntegrationTests/RedisTestsNamedContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PubComp.RedisRepo.IntegrationTests
+{
+    [TestClass]
+    public class RedisTestsNamedContext : RedisTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            redisContext = RedisTestContext.Retry(()
+                => new RedisTestContext(nameof(RedisTestsNamedContext), db: 1), 5);
+            ClearDb(redisContext, TestContext);
+
+            redisContext.Delete(TestContext.TestName);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            (redisContext as RedisTestContext)?.Connection?.Dispose();
+        }
+    }
+}
---
+++ IntegrationTests/RedisTestsUnnamedContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PubComp.RedisRepo.IntegrationTests
+{
+    [TestClass]
+    public class RedisTestsUnnamedContext : RedisTests
+    {
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            redisContext = RedisTestContext.Retry(()
+                => new RedisTestContext(null, db: 2), 5);
+            ClearDb(redisContext, TestContext);
+
+            redisContext.Delete(TestContext.TestName);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            (redisContext as RedisTestContext)?.Connection?.Dispose();
+        }
+    }
+}
